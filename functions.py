afba6cd3f386d74078ca04973ef907a821ba29ee
Cristopher Delgado <delgadocristopher524@gmail.com>
2024-02-06 14:25:51 -0800

    modified evaluate function

--- functions.py
+++ functions.py
@@ -309,17 +309,17 @@ def model_evaluate(model, train_gen, test_gen, val_gen):
     # Evaluate on the training set
     train_results = model.evaluate(train_gen)
     train_metrics = ['Train'] + train_results[:]
-    results = results.append(dict(zip(columns, train_metrics)), ignore_index=True)
+    results = pd.concat([results, pd.DataFrame([dict(zip(columns, train_metrics))])], ignore_index=True)
     
     # Evaluate on the test set
     test_results = model.evaluate(test_gen)
     test_metrics = ['Test'] + test_results[:]
-    results = results.append(dict(zip(columns, test_metrics)), ignore_index=True)
+    results = pd.concat([results, pd.DataFrame([dict(zip(columns, test_metrics))])], ignore_index=True)
     
     # Evaluate on the validation set
     validation_results = model.evaluate(val_gen)
     val_metrics = ['Validation'] + validation_results[:]
-    results = results.append(dict(zip(columns, val_metrics)), ignore_index=True)
+    results = pd.concat([results, pd.DataFrame([dict(zip(columns, val_metrics))])], ignore_index=True)
     
     # Lets modify the Precision, Recall, Accuracy to percentages
     results['Precision'] = results['Precision']*100
