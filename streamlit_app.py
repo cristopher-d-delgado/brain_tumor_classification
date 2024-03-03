da53c47a78b4cc6982cd449b1e954a47155129c9
Cristopher Delgado <delgadocristopher524@gmail.com>
2024-03-03 15:41:35 -0800

    initialized prediction python file to use to use in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
+from keras.models import load_model
 
 st.title("Brain Tumor Classification with Magnetic Resonance Imaging")
 
-st.write("Hello World!")
+@st.cache_resource
+def load_model():
+    # Load classifier for mri images
+    model = load_model("models/op_model1_aug.keras")
+    
+    return model
