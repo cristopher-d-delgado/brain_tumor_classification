791b395b065f5528bdfa2ad46baefdaa4eb81656
Cristopher Delgado <delgadocristopher524@gmail.com>
2024-03-09 17:06:22 -0800

    added LIME to streamlit

--- predict.py
+++ predict.py
@@ -2,52 +2,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from PIL import ImageOps, Image
 import streamlit as st
 import numpy as np
-
-# def preprocess_image_gen(img_dims):
-#     """Preprocesses a single image based on the same preprocessing steps defined in process_data function.
-    
-#     Parameters:
-#     - img_path (str): Path to the image file.
-#     - img_dims (int): Specify the image dimensions in a single number. Ex: 128 will produce (128, 128).
-    
-#     Returns:
-#     - img_array (numpy.ndarray): Preprocessed image as a numpy array.
-#     """
-    
-#     # Creat instance of datagenerator
-#     data_generator = ImageDataGenerator(rescale=1./255)
-    
-#     # Load and preprocess the image using the ImageDataGenerator
-#     img = data_generator.flow_from_directory(
-#         directory=img_path,
-#         target_size=(img_dims, img_dims),
-#         batch_size=1,
-#         class_mode=None,  # Since we are not using any labels
-#         shuffle=False  # No need to shuffle since we're processing a single image
-#     )
-    
-#     image = next(img)
-    
-#     return image
-
-# def predict_image_class(model, img_path, img_dims):
-#     """Predicts the class of an image using a pre-trained model.
-    
-#     Parameters:
-#     - model (tf.keras.Model): Pre-trained Keras model.
-#     - img_path (str): Path to the image file.
-#     - img_dims (int): Specify the image dimensions in a single number. Ex: 128 will produce (128, 128).
-    
-#     Returns:
-#     - prediction (numpy.ndarray): Predicted class probabilities.
-#     """
-#     # Preprocess the image
-#     img_array = load_and_preprocess_image(img_path, img_dims)
-    
-#     # Make predictions
-#     prediction = model.predict(img_array)
-    
-#     return prediction
+from keras.models import load_model
 
 def classify(image, model, class_names):
     """
@@ -85,3 +40,13 @@ def classify(image, model, class_names):
     predicted_class_name = class_names[predicted_class_index]
     
     return predicted_class_name, predicted_class_prob
+
+# Preprocess data into array 
+def preprocess_image(image, target_size=(128, 128)):
+    # Resize image 
+    img = image.resize(target_size)
+    # Convert to RGB
+    img_rgb = img.convert('RGB')
+    # Convert to array and normalize
+    img_array = np.array(img_rgb) / 255.0
+    return img_array
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from keras.models import load_model
-from predict import classify
+from predict import classify, preprocess_image
 from PIL import Image, ImageOps
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
 import numpy as np
+import matplotlib.pyplot as plt
+import io
 
 # Title app
 st.title("Brain Tumor Classification with Magnetic Resonance Imaging")
@@ -19,6 +23,15 @@ def load_keras_model(path):
     # Load classifier for mri images
     model = load_model(path)
     return model
+
+# Cache Lime explainer
+@st.cache_resource
+def load_lime_explainer():
+    """
+    Load LimeImageExplainer
+    """
+    explainer = lime_image.LimeImageExplainer(random_state=42)
+    return explainer
 
 # Load classifier
 aug_path = "models/op_model1_aug.keras"
@@ -53,6 +66,77 @@ if file is not None:
     # Write classification
     st.write(f"### The Brain MRI image is most likely a {class_name[0]} instance")
     st.write(f"### The probability that the image is a {class_name[0]} instance is: {probability}%")
+
+    # Lime Explanation
+    with st.expander("See Lime Explanation"):
+        with st.container():
+            # Divide container into 2 columns
+            col_1, col_2 = st.columns(2)
+            
+            # Load Lime explainer
+            explainer = load_lime_explainer()
+            
+            # Define image we want to predict
+            image = Image.open(file)
+            
+            # Preprocess the image for lime explanation and model prediction
+            img = preprocess_image(image)
+            
+            # Develop local model explanation
+            explanation = explainer.explain_instance(
+                image=img,
+                classifier_fn=aug_model.predict,
+                top_labels=4,
+                num_samples=2000,
+                hide_color=0,
+                random_seed=42
+            )
+
+            # Obtain mask and image from explainer
+            temp, mask = explanation.get_image_and_mask(
+                explanation.top_labels[0],  # Using the top predicted label for visualization
+                positive_only=True,
+                num_features=5, 
+                hide_rest=True, 
+                min_weight=0.1
+            )
+
+            # Display mask and image in column 2
+            with col_1:
+                st.image(mark_boundaries(temp / 2 + 0.5, mask), caption="Lime Explanation", use_column_width=True)
+            
+            # Using the same explainer get a heatmap version that explains the areas that contribute most to that decision
+            # Select the top label
+            with col_2:
+                # Select the top label
+                ind = explanation.top_labels[0]
+        
+                # Map each explanation weight to the corressponding superpixel
+                dict_heatmap = dict(explanation.local_exp[ind])
+                heatmap = np.vectorize(dict_heatmap.get)(explanation.segments)
+
+                # Display heatmap 
+                # Display normalized heatmap with colorbar
+                plt.imshow(heatmap, cmap='RdBu', vmin=-heatmap.max(), vmax=heatmap.max())
+                plt.colorbar()
+                plt.title("Heatmap")
+                plt.axis('off')  # Hide axes
+                plt.show()
+                
+                # Save plot as bytes
+                buf = io.BytesIO()
+                plt.savefig(buf, format='png')
+                buf.seek(0)
+
+                # Display heatmap using Streamlit
+                st.image(buf, caption='Heatmap', use_column_width=True)
+                
+                # Delete buf object to free up memory
+                del buf
+        # st.image(mark_boundaries(temp / 2 + 0.5, mask), caption="Lime Explanation", use_column_width=True)
+        # st.image(heatmap)
+        
+        
 
 #### Make a section talking about the model 
 
